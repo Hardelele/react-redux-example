e53bb9435c1e49e3405f3f87d75297ac7e6538c7
Hardelele <Hardelele@yahoo.com>
2020-03-21 18:52:39 +0300

    Add BrowserRouter

--- src/Application.js
+++ src/Application.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './css/application.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {Registration} from "./components/Registration";
+import {Auth} from "./components/Auth";
+import {Header} from "./components/Header";
 
 function Application() {
   return (
-    <div className="main-application-container">
-
-    </div>
+      <BrowserRouter>
+        <div className="main-application-container">
+            <Header/>
+            <Switch>
+                <Route path="/register" exact component={Registration}/>
+                <Route path="/auth" component={Auth}/>
+            </Switch>
+        </div>
+      </BrowserRouter>
   );
 }
 
