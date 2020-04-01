9e2fd7f1251b8f796c7ce14c1fd7be9e0170b92d
Hardelele <Hardelele@yahoo.com>
2020-04-01 20:08:23 +0300

    Add rootReducer to store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './Application';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+import {rootReducer} from "./redux/rootReducer";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
