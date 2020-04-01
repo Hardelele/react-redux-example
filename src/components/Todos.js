a96d755c980cee26f002671ddaee784fb5417ae7
Hardelele <Hardelele@yahoo.com>
2020-04-01 19:46:57 +0300

    Add content to Todos Component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,30 @@
 import {Button} from "react-bootstrap";
 import React from "react";
 import '../css/todos.css'
+import PostForm from "./PostForm";
+import PostList from "./PostList";
+import FetchedList from "./FetchedList";
 
 export const Todos = () => {
     return (
         <div className="todos-container">
-            <Button variant="primary" id="todos-button">todos</Button>
+            <div className="row">
+                <div className="col">
+                    <PostForm/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h1>Post List</h1>
+                    <PostList posts={[1, 2, 3]}/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h1>Fetched List</h1>
+                    <FetchedList posts={[]}/>
+                </div>
+            </div>
         </div>
     );
 };
