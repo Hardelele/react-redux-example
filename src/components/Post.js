7fc6d3cbff5d35209f69d1a110bc8fcffc529f4d
Hardelele <Hardelele@yahoo.com>
2020-04-01 23:33:32 +0300

    Component render post.title

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,7 @@ export default ({post}) => {
     return (
         <div className="card">
             <div className="card-body">
-                <h5 className="card-title">{post}</h5>
+                <h5 className="card-title">{post.title}</h5>
             </div>
         </div>
     )
