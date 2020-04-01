ecf95f985c8b71be47e8216e881592934a55475f
Hardelele <Hardelele@yahoo.com>
2020-04-02 00:06:35 +0300

    Add async posts from api

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,16 @@
-import {CREATE_POST} from "./types";
+import {CREATE_POST, FETCHED_POSTS} from "./types";
+import axios from "axios";
 
 export function createPost(post) {
     return {
         type: CREATE_POST,
         payload: post
+    }
+}
+
+export function fetchPosts() {
+    return async dispatch => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+        dispatch({type: FETCHED_POSTS, payload: response.data})
     }
 }
